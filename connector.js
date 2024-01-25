83ac9d8777bfb5b045b90dd0dcf8b15af3976621
leonsillano <leonsillano@yahoo.fr>
2024-01-25 16:44:02 +0700

    improvements

--- connector.js
+++ connector.js
@@ -19,12 +19,14 @@ const RUNNING = "RUNNING"
 var gama_ws_port = DEFAULT_GAMA_WS_PORT
 var gama_ip_address = DEFAULT_GAMA_IP_ADDRESS
 
-var connection_state
+var connection_state = DISCONNECTED
 var gama_state
 var model_file 
 var experiment_name 
 var experiment_id
 var current_expression
+var current_nb_step
+var current_nb_step_back
 
 var socket = createWebSocketClient()
 init()
@@ -74,6 +76,22 @@ function send_ask() {
     }
 }
 
+function send_step() {
+    return {
+        "type": "step",
+        "exp_id": experiment_id,
+        "nb_step": current_nb_step
+      }
+}
+
+function send_step_back() {
+    return {
+        "type": "stepBack",
+        "exp_id": experiment_id,
+        "nb_step": current_nb_step_back
+      }
+}
+
 // Setters
 function setConnectionState(newState) {
     connection_state = newState
@@ -88,7 +106,7 @@ function setGamaState(newState) {
     gama_state = newState
     state_html = document.querySelector("#gama-state")
     switch (gama_state) {
-        case UNKNOWN: state_html.innerHTML = "?"; state_html.style = "color: black;"; break;
+        case UNKNOWN: state_html.innerHTML = "?"; state_html.style = "color: white;"; break;
         case NONE: state_html.innerHTML = "NONE (no experiment running)"; state_html.style = "color: red;"; break;
         case NOTREADY: state_html.innerHTML = "NOTREADY (the experiment is not ready)"; state_html.style = "color: red;"; break;
         case PAUSED: state_html.innerHTML = "PAUSED (the experiment is paused)"; state_html.style = "color: orange;"; break;
@@ -97,7 +115,9 @@ function setGamaState(newState) {
 }
 
 function createWebSocketClient() {
-    let gama_socket = new WebSocket('ws://'+gama_ip_address+':'+gama_ws_port);
+    let gama_full_address = 'ws://'+gama_ip_address+':'+gama_ws_port
+    document.querySelector("#gama-address").innerHTML = gama_full_address;
+    let gama_socket = new WebSocket(gama_full_address);
 
     gama_socket.onopen = function() {
         console.log("-> Connected to Gama Server");
@@ -145,6 +165,10 @@ function createWebSocketClient() {
 }
 
 function load() {
+    if (connection_state == DISCONNECTED) {
+        logRequest("Gama Server is not connected")
+        return
+    }
     if ([NOTREADY, PAUSED, RUNNING].includes(gama_state)) {
         logRequest("Could not send Load since Gama Server is already running an experiment")
     }
@@ -158,16 +182,24 @@ function load() {
 }
 
 function start() {
+    if (connection_state == DISCONNECTED) {
+        logRequest("Gama Server is not connected")
+        return
+    }
     if ([PAUSED].includes(gama_state)) {
         socket.send(JSON.stringify(play_experiment()))
         logRequest(play_experiment())
     }
     else {
-        logRequest("Could not send Play since Gama Server is not currently paused")
+        logRequest("Could not send Play since Gama Server is not currently loaded or paused")
     }
 }
 
 function pause() {
+    if (connection_state == DISCONNECTED) {
+        logRequest("Gama Server is not connected")
+        return
+    }
     if ([RUNNING].includes(gama_state)) {
         socket.send(JSON.stringify(pause_experiment()))
         logRequest(pause_experiment())
@@ -178,6 +210,10 @@ function pause() {
 }
 
 function end() {
+    if (connection_state == DISCONNECTED) {
+        logRequest("Gama Server is not connected")
+        return
+    }
     if ([RUNNING, PAUSED].includes(gama_state)) {
         socket.send(JSON.stringify(stop_experiment()))
         logRequest(stop_experiment())
@@ -188,6 +224,10 @@ function end() {
 }
 
 function expression() {
+    if (connection_state == DISCONNECTED) {
+        logRequest("Gama Server is not connected")
+        return
+    }
     if ([RUNNING, PAUSED].includes(gama_state)) {
         socket.send(JSON.stringify(send_expression()))
         logRequest(send_expression())
@@ -198,6 +238,10 @@ function expression() {
 }
 
 function ask() {
+    if (connection_state == DISCONNECTED) {
+        logRequest("Gama Server is not connected")
+        return
+    }
     if ([RUNNING, PAUSED].includes(gama_state)) {
         socket.send(JSON.stringify(send_ask()))
         logRequest(send_ask())
@@ -207,22 +251,45 @@ function ask() {
     }
 }
 
-function setModelFile() {
-    model_file = document.querySelector("#model-file-input").value 
+function step() {
+    if (connection_state == DISCONNECTED) {
+        logRequest("Gama Server is not connected")
+        return
+    }
+    if ([RUNNING, PAUSED].includes(gama_state)) {
+        socket.send(JSON.stringify(send_step()))
+        logRequest(send_step())
+    }
+    else {
+        logRequest("Could not send Step since Gama Server is not currently running")
+    }
 }
 
-function setIpAddress() {
+function stepBack() {
+    if (connection_state == DISCONNECTED) {
+        logRequest("Gama Server is not connected")
+        return
+    }
+    if ([RUNNING, PAUSED].includes(gama_state)) {
+        socket.send(JSON.stringify(send_step_back()))
+        logRequest(send_step_back())
+    }
+    else {
+        logRequest("Could not send Step Back since Gama Server is not currently running")
+    }
+}
+
+
+function setAddress() {
     gama_ip_address = document.querySelector("#ip-address-input").value
-    restartConnector()
-}
-
-function setWebSocketPort() {
     gama_ws_port = document.querySelector("#ws-port-input").value 
     restartConnector()
 }
 
-function setExperimentName() {
-    experiment_name = document.querySelector("#experiment-name-input").value 
+
+function setExperiment() {
+    experiment_name = document.querySelector("#experiment-name-input").value
+    model_file = document.querySelector("#model-file-input").value 
 }
 
 function sendExpression() {
@@ -235,6 +302,16 @@ function sendAsk() {
     current_agent = document.querySelector("#agent-ask-input").value
     current_args = document.querySelector("#args-ask-input").value
     ask()
+}
+
+function sendStep() {
+    current_nb_step = document.querySelector("#nb-step-input").value
+    step()
+}
+
+function sendStepBack() {
+    current_nb_step = document.querySelector("#nb-step-back-input").value
+    stepBack()
 }
 
 function init() {
